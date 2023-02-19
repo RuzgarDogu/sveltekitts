86f9394dea099aaef87f4d86d84ec650a2c06fa7
emre <emre@uniqeurope.com>
2023-02-19 21:09:47 +0300

    Test

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,5 +3,5 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<h1>About</h1>
+<h1>About Page</h1>
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid culpa quia amet, quod, perspiciatis quis error dolor rerum iste quos cupiditate nobis recusandae, ipsa consequuntur! Id quia animi ex voluptates.
